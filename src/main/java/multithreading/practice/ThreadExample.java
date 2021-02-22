087792e0957071b4f02bb628a7cc876f9322012d
Alex Korzun <koleksandr4@gmail.com>
2021-02-22 15:29:00 +0200

    created 'Thread example class'

--- src/main/java/multithreading/practice/ThreadExample.java
+++ src/main/java/multithreading/practice/ThreadExample.java
@@ -1,8 +1,18 @@
 package multithreading.practice;
 
 public class ThreadExample extends Thread {
+    private final Counter counter;
+
+    public ThreadExample(Counter counter) {
+        this.counter = counter;
+    }
+
     @Override
     public void run() {
-        super.run();
+        System.out.println("Thread example started working...");
+        for (int i = 0; i < 100; i++) {
+            counter.setCounter(i);
+            System.out.println(counter);
+        }
     }
 }
